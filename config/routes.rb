75719724d1b6c3429dee1c6ae6717dba61d74bc0
Adrian <adrianleon0100@gmail.com>
2024-09-06 09:16:21 -0600

    Update navigation links in application layout and add my comparisons sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   post "save_comparison", to: "properties#save_comparison"
   get "export_comparison", to: "properties#export_comparison", defaults: { format: "xml" }
   resources :comparisons, only: [ :index ]
+  resources :comparisons, only: [ :show ]
 end
